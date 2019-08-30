22e4260eea2e6b16ee18643e0af82c0415f9a24b
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-08-30 11:30:12 +0530

    Run Length Encoding

---
+++ ROB/run_length_encoding.py
@@ -0,0 +1,20 @@
+def algo(arr):
+    count = 1
+    prev = arr[0]
+    ans = []
+    for ele in arr[1:]:
+        if ele == prev:
+            count += 1
+        else:
+            ans.extend((prev, count))
+            count  = 1
+        prev = ele
+    else:
+        ans.extend((prev, count))
+    return ans
+
+
+if __name__ == '__main__':
+    ip = tuple(map(int, input('enter the ip sequence(without spaces): ')))
+    op = algo(ip)
+    print('output sequence:', op)
