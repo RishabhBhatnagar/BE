421dc0393c66a7993504f355682d736563f6b5a9
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-09-25 23:15:02 +0530

    Add DSIP o/p And Prewitt Code

--- DSIP/EdgeDetection/code.py
+++ DSIP/EdgeDetection/code.py
@@ -7,6 +7,11 @@ HSOBEL_WEIGHTS = np.array([[1, 2, 1],
                            [-1, -2, -1]]) / 4.0
 VSOBEL_WEIGHTS = HSOBEL_WEIGHTS.T
 
+HPREWITT_WEIGHTS = np.array([[1, 1, 1],
+                             [0, 0, 0],
+                             [-1, -1, -1]]) / 4.0
+VPREWITT_WEIGHTS = HSOBEL_WEIGHTS.T
+
 
 def convolve(img, mask):
     m, n, = img.shape
@@ -25,8 +30,18 @@ def sobel(img):
     sobel_v = convolve(img, VSOBEL_WEIGHTS)
     return abs(sobel_h + 1j * sobel_v)
 
+def prewitt(img):
+    img = img / 255         # normalizing 
+    prewitt_h = convolve(img, HPREWITT_WEIGHTS)
+    prewitt_v = convolve(img, VPREWITT_WEIGHTS)
+    return abs(prewitt_h + 1j * prewitt_v)
 
-img = cv.imread('img.png', 0)
-ans = sobel(img)
-cv.imshow('end', ans)
-cv.waitKey(3000)
+
+if __name__ == '__main__':
+    img = cv.imread('img.png', 0)
+    cv.imshow('sobel', sobel(img))
+    cv.waitKey(3000)
+    cv.imshow('prewitt', prewitt(img))
+    cv.waitKey(3000)
+
+
