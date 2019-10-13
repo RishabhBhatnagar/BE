4be0cf43de0a1eaf7b49d9b35ba32c2c5ed2ab90
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2019-10-13 19:48:50 +0530

    Rename Files, Restructure Directory

--- MADTL/cdma/code.py
+++ MADTL/cdma/code.py
@@ -1,29 +1,45 @@
 from functools import reduce
 
 
-
-Fn = 1 << 22  # globally(publically) available frame number.
-
-
-
-def split_n(num, n):
-    ones = int('0b' + '1' * n, 2)
-    lh = ones & num
-    rh = (num - lh) >> n
-    return lh, rh
-
-
-def A3(rand, key):
-    rand_l, rand_r = split_n(rand, 64)
-    key_l, key_r = split_n(key, 64)
-    l_xor = rand_l ^ key_r
-    r_xor = rand_r ^ key_l
-
-    final_xor = l_xor ^ r_xor
-    res = reduce(int.__xor__, split_n(final_xor, 32))
-    return res
+class User:
+     def __init__(self, code):
+          # code is a list of 1 and -1.
+          # All the users has same number of bits in the code.
+          # with length equal to number of users.
+          self.code = code
+     def __mul__(self, num):
+          return [num * bit for bit in self.code]
+     def __add__(self, oth):
+          return [a + b for a, b in zip(self.code, oth)]
+     def __repr__(self):
+          return self.code.__str__()
+     __rmul__ = __mul__
 
 
-def A5(Kc, pt, Fn = 1 << 22):
-    pseudo_random 
-print(A3(340282366920938463463374607431768211456, 345282366920998463463374607431768211456))
+def algo():
+     avail_codes = [
+          [1] * 4,
+          [1, -1, 1, -1],
+          [1, 1, -1, -1],
+          [1, -1, -1, 1]
+     ]
+     users = [User(code) for code in avail_codes]
+     data = tuple(map(int, input("Enter the data of the four stations: ").split()))
+     garbled = [num * user for num, user in zip(data, users)]
+     final_code = User([sum(codes) for codes in zip(*garbled)])
+     which_channel = int(input('Enter the channel you want to listen to[0-4]: '))
+     decoded = sum([num * user for num, user in zip(final_code.code, users[which_channel].code)]) >> 2
+     print(decoded, 'is the data.')
+     
+if __name__ == '__main__':
+     algo()
+
+
+
+
+"""
+OUTPUT:
+Enter the data of the four stations: 48 17 50 87
+Enter the channel you want to listen to[0-4]: 3
+87 is the data.
+"""
---
+++ MADTL/gsm/code.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+
+
+Fn = 1 << 22  # globally(publically) available frame number.
+
+
+
+def split_n(num, n):
+    ones = int('0b' + '1' * n, 2)
+    lh = ones & num
+    rh = (num - lh) >> n
+    return lh, rh
+
+
+def A3(rand, key):
+    rand_l, rand_r = split_n(rand, 64)
+    key_l, key_r = split_n(key, 64)
+    l_xor = rand_l ^ key_r
+    r_xor = rand_r ^ key_l
+
+    final_xor = l_xor ^ r_xor
+    res = reduce(int.__xor__, split_n(final_xor, 32))
+    return res
+
+ 
+
+print(A3(340282366920938463463374607431768211456, 345282366920998463463374607431768211456))
